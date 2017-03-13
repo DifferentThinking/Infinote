e563243f5d42c6c0634fe6104fa58d8b029872ef
Georgi Karaboichev <djorkaevhell@abv.bg>
2017-03-13 22:53:56 +0200

    Init

---
+++ Infinote/app/src/main/java/com/infinote/differentthinking/infinote/ListNotesActivity.java
@@ -0,0 +1,16 @@
+package com.infinote.differentthinking.infinote;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListNotesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_notes);
+
+
+    }
+}
