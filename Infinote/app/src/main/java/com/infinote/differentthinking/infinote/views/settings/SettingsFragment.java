7f897b17281c11d3e32b67cf494b1193abc9676f
Georgi Karaboichev <djorkaevhell@abv.bg>
2017-05-29 18:46:41 +0300

    Fix settings activity

--- Infinote/app/src/main/java/com/infinote/differentthinking/infinote/views/settings/SettingsFragment.java
+++ Infinote/app/src/main/java/com/infinote/differentthinking/infinote/views/settings/SettingsFragment.java
@@ -23,7 +23,7 @@ public class SettingsFragment extends Fragment implements SettingsContract.View 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_account_settings, container, false);
 
         return view;
     }
